fbef35c9e000928ef3f9aaae23a1ab88277ac537
Ahuhu <ahoho@example.com>
2023-06-12 23:40:24 +0700

    camera

--- ahihi.py
+++ ahihi.py
@@ -1,9 +1,25 @@
-print('bao dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
-print('binh dep trai khoai to')
+# print('bao dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+# print('binh dep trai khoai to')
+
+import cv2
+
+cap = cv2.VideoCapture(0)
+# cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow('Camera',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
